29fb0ef70493f54acd0ecb13936f3dcf41b109d4
dcapwell <d.capwell@sisa.samsung.com>
2013-01-27 12:27:14 -0800

    working on a release script.  fixed json bugs in package.json

---
+++ release.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+FILE=RELEASE_NOTES
+TMP_FILE=${FILE}.tmp
+NAME=$(cat package.json | grep name | sed -e "s/.*name.*\"\(.*\)\",/\1/")
+VERSION=$(cat package.json | grep version | sed -e "s/.*version.*\"\(.*\)\",/\1/")
+TAG=$NAME-$VERSION
+
+echo "$TAG" > ${TMP_FILE}
+git describe --abbrev=0 2> /dev/null 1> /dev/null \
+  && git log --pretty="%x09* [%h] %s." $(git describe --abbrev=0)..HEAD >> ${TMP_FILE} \
+  || git log --pretty="%x09* [%h] %s." >> ${TMP_FILE}
+
+echo "" >> ${TMP_FILE}
+
+if [ -e $FILE ]; then
+  cat ${FILE} >> ${TMP_FILE}
+fi
+
+mv ${TMP_FILE} $FILE
+
+#git commit -a -m "[release-script] Updated release notes for version ${VERSION}"
+#git tag -a $TAG -m "[auto-release] version $VERSION"
