f59f22a20d8b88c4ff3f10ab26b27d7cef44b62e
dcapwell <d.capwell@sisa.samsung.com>
2013-01-27 00:10:04 -0800

    made the formatLine return a new Row class obj rather than new obj each time.  removed unused variables and added method to removed unneeded data points after parsing

--- test/gedcom-test.js
+++ test/gedcom-test.js
@@ -1,7 +1,17 @@
 var assert = require('assert'),
+  util = require('util'),
   ged = require('../lib/gedcom.js');
 
 ged.parse('test/Case002-Repositories.ged', function(e) {
-  console.log(e);
-  assert.equal(e.children.length, 10);
+  e.simplify();
+  console.log(util.inspect(e, false, null, true));
+  assert.ok(e.HEAD);
+  assert.ok(e.HEAD.SOUR);
+  assert.ok(e.HEAD.SOUR.NAME);
+  assert.ok(e.HEAD.SOUR.VERS);
+  assert.equal(e.HEAD.SOUR.NAME.value, "MyCorporation Gedcom Generator");
+  assert.equal(e.HEAD.SOUR.VERS.value, "5.2.18.0");
+  assert.ok(e.REPO);
+  assert.ok(e.INDI);
+  assert.ok(e.SUBM);
 });
